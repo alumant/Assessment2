8ac141cd9d7a5b56e951d517c5236d43212efa9f
alumant <118276305+alumant@users.noreply.github.com>
2024-02-02 15:32:32 +0200

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array {
+        public static void longest(String[] ["the", "quick", "brown", "fox", "ate", "my", "chickens") {
+
+
+            String longestString = strings[0];
+            for (int i = 1; i < strings.length; i++) {
+                if (strings[i].length() > longestString.length()) {
+                    longestString = strings[i];
+                }
+            }
+        }
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
